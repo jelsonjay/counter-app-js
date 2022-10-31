05ce145ac82547bae92499d1be2bd1f8bd96a7d9
Jelson J <jelson20@outlook.com>
2022-10-31 19:41:39 +0000

    commit update

--- app.js
+++ app.js
@@ -3,8 +3,19 @@
 const counter = document.querySelector('.counter');
 const increse = document.querySelector('.increse');
 const decrese = document.querySelector('.decrese');
-const sort = document.querySelector('.sort');
+const reset = document.querySelector('.reset');
 
 increse.addEventListener('click', () => {
 	counter.innerHTML++;
+	counter.style.color = '#7deb9e';
+});
+
+decrese.addEventListener('click', () => {
+	counter.innerHTML--;
+	counter.style.color = '#e67d7d';
+});
+
+reset.addEventListener('click', () => {
+	counter.innerHTML = 0;
+	counter.style.color = '#adabab';
 });
